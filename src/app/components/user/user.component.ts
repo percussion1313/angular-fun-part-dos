ae836f9854c3eed631a9b912108ee22462e6d5ed
percussion1313 <peterjacobgrant@gmail.com>
2019-03-24 14:25:13 -0500

    playin with some stuff

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,16 +1,34 @@
 import  { Component } from '@angular/core';
-
+import { User } from '../../models/User'
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html'
 })
 
 export class UserComponent {
-    firstName = 'Peter';
-    lastName = 'Sup';
-    age = 26;
+    user: User
+    
 
     constructor() {
-             
+        this.user = {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 87,
+            address: {
+                street: '123 main st',
+                city: 'Los Angeles',
+                state: 'California'
+            }
+        }
     }
+
+    sayHello() {
+        console.log(`Hello`)
+    }
+
+    hasBirthday() {
+       
+    }
+
+   
 }
